79724accae65047c979e510ab34a81ef76f6e50a
will <dodge.ttu@gmail.com>
2019-03-20 08:41:25 -0500

    update

--- linear_example.py
+++ linear_example.py
@@ -17,10 +17,14 @@ import cv2
 labels = ['dog', 'cat', 'panda']
 np.random.seed(1)
 
-
 # Randomly initialize our weight matrix and bias vector -- in a real world situation these values would be the product
 # of training and learned with our model but for the sake of this example the values will be random.
 W = np.random.randn(3, 3072)
+b = np.random.randn(3)
+
+# Load our example image, resize, then flatten into our "feature vector" representation.
+orig = cv2.imread("/home/will/seedling-detection/beagle.png")
+image = cv2.resize(orig, (32, 32)).flatten()
 
 
 
